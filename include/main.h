8943c50f2900dff7158074e8bf9a2ccdd100b56a
Steve Fan <19037626d@connect.polyu.hk>
2020-05-05 19:07:35 +0800

    init

---
+++ include/main.h
@@ -0,0 +1,8 @@
+﻿// comp2432_group_assignment.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
---
+++ src/main.c
@@ -0,0 +1,12 @@
+﻿// comp2432_group_assignment.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
